659baf1995eea2d5fd1b24a961448e398ed72e5f
macavall <43223084+macavall@users.noreply.github.com>
2023-02-03 18:12:30 -0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(V4DepInjThreadsMem562.Startup))]
+
+namespace V4DepInjThreadsMem562
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<ITestService, TestService>();
+        }
+    }
+}
